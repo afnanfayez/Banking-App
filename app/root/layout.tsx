a3a99f015eaa557d88a1c374b793b9f560cd17b9
afnan <afnan232003@gmail.com>
2026-01-12 12:01:09 +0200

    implement sidebar logo and style mapped links

--- app/root/layout.tsx
+++ app/root/layout.tsx
@@ -1,10 +1,16 @@
+import Sidebar from "@/components/ui/ui/Sidebar";
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return(<main>
-    SIDE BAR
+  const loggedIn = {
+    firstName: 'Adrien', lastName: 'DUPONT'
+  }
+  return (<main className="flex h-screen w-full font-inter">
+    <Sidebar user={loggedIn as any} />
     {children}
   </main>
   );
--- constants/index.ts
+++ constants/index.ts
@@ -1,7 +1,7 @@
 export const sidebarLinks = [
   {
     imgURL: "/icons/home.svg",
-    route: "/",
+    route: "/root",
     label: "Home",
   },
   {
