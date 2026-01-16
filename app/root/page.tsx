9c0a11b766cd30557d983491c8c213982b2c6ec9
afnan <afnan232003@gmail.com>
2026-01-16 04:05:01 +0200

    Implement Bank Card

--- app/root/page.tsx
+++ app/root/page.tsx
@@ -4,7 +4,22 @@ import TotalBalanceBox from "@/components/ui/ui/TotalBalanceBox";
 
 
 const Home = () => {
-  const loggedIn = { firstName: 'Afnan' ,lastName:'Zeiti',email:'afnanzeiti@gmail.com'}
+  const loggedIn = {
+    $id: '1',
+    firstName: 'Afnan',
+    lastName: 'Zeiti',
+    email: 'afnanzeiti@gmail.com',
+    userId: '1',
+    dwollaCustomerUrl: '',
+    dwollaCustomerId: '',
+    address1: '',
+    city: '',
+    state: '',
+    postalCode: '',
+    dateOfBirth: '',
+    ssn: ''
+  }
+
   return (
     <section className="home">
       <div className="home-content">
@@ -26,9 +41,50 @@ const Home = () => {
         RECENT TRANSACTIONS
 
       </div>
-      <RightSidebar user={loggedIn}
-      transactions={[]}
-      banks={[]}/>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[
+          {
+            $id: '1',
+            accountId: '1',
+            bankId: '1',
+            accessToken: '',
+            fundingSourceUrl: '',
+            userId: '1',
+            sharableId: '',
+            id: '1',
+            availableBalance: 123.50,
+            currentBalance: 123.50,
+            officialName: 'Main Account',
+            mask: '1234',
+            institutionId: '',
+            name: 'Plaid Gold Standard',
+            type: 'depository',
+            subtype: 'checking',
+            appwriteItemId: '1'
+          },
+          {
+            $id: '2',
+            accountId: '2',
+            bankId: '2',
+            accessToken: '',
+            fundingSourceUrl: '',
+            userId: '1',
+            sharableId: '',
+            id: '2',
+            availableBalance: 500.50,
+            currentBalance: 500.50,
+            officialName: 'Savings Account',
+            mask: '5678',
+            institutionId: '',
+            name: 'Plaid Silver Standard',
+            type: 'depository',
+            subtype: 'savings',
+            appwriteItemId: '2'
+          }
+        ]}
+      />
     </section>
   )
 };
