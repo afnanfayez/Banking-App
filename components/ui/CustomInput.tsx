14b512d1b305bffc69c83bb9933dd4ec7db3d6e6
afnan <afnan232003@gmail.com>
2026-01-22 11:27:11 +0200

    enhance sign-up form with additional fields and dynamic zod schema

--- components/ui/CustomInput.tsx
+++ components/ui/CustomInput.tsx
@@ -1,41 +1,43 @@
-import { Label } from "@radix-ui/react-label"
-import { FormControl, FormField, FormLabel, FormMessage } from "./form"
-import { Input } from "./input"
-import {Control, FieldPath} from 'react-hook-form'
-import {z} from 'zod'
-import { authformSchema } from "@/lib/utils"
+import { Label } from "@radix-ui/react-label";
+import { FormControl, FormField, FormLabel, FormMessage } from "./form";
+import { Input } from "./input";
+import { Control, FieldPath, FieldValues } from "react-hook-form";
 
-
-
-interface CustomInput {
-  control:Control<z.infer<typeof authformSchema>> ,
-   name :FieldPath<z.infer<typeof authformSchema>> ,
-   label :string ,
-   placeholder :string
+interface CustomInputProps<T extends FieldValues> {
+  control: Control<T>;
+  name: FieldPath<T>;
+  label: string;
+  placeholder: string;
 }
 
-const CustomInput =({control ,name ,label,placeholder}:CustomInput)=>{
-    return (
-      <FormField
-                        control={control}
-                        name={name}
-                        render={({ field }) => (
-                         <div className='form-item'>
-                            <FormLabel className="form-label">
-                                {label}
-                                </FormLabel>
-                                <div className="flex w-full flex-col" >
-                                <FormControl >
-                                    <Input placeholder={placeholder} className="input-class"
-                                    type ={name==='password' ? 'password' : 'text'}
-                                    {...field} />
-                                </FormControl>
-                                <FormMessage className="form-message mt-2" />
-                                </div>
-                         </div>
-                        )}
-                    />
-    )
-}
+const CustomInput = <T extends FieldValues>({ 
+  control, 
+  name, 
+  label, 
+  placeholder 
+}: CustomInputProps<T>) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <div className="form-item">
+          <FormLabel className="form-label">{label}</FormLabel>
+          <div className="flex w-full flex-col">
+            <FormControl>
+              <Input
+                placeholder={placeholder}
+                className="input-class"
+                type={name === "password" ? "password" : "text"}
+                {...field}
+              />
+            </FormControl>
+            <FormMessage className="form-message mt-2" />
+          </div>
+        </div>
+      )}
+    />
+  );
+};
 
-export default CustomInput
+export default CustomInput;
--- lib/utils.ts
+++ lib/utils.ts
@@ -40,22 +40,22 @@ export const formatDateTime = (dateString: Date) => {
 
   const formattedDateTime: string = new Date(dateString).toLocaleString(
     "en-US",
-    dateTimeOptions
+    dateTimeOptions,
   );
 
   const formattedDateDay: string = new Date(dateString).toLocaleString(
     "en-US",
-    dateDayOptions
+    dateDayOptions,
   );
 
   const formattedDate: string = new Date(dateString).toLocaleString(
     "en-US",
-    dateOptions
+    dateOptions,
   );
 
   const formattedTime: string = new Date(dateString).toLocaleString(
     "en-US",
-    timeOptions
+    timeOptions,
   );
 
   return {
@@ -98,7 +98,7 @@ export function formUrlQuery({ params, key, value }: UrlQueryParams) {
       url: window.location.pathname,
       query: currentUrl,
     },
-    { skipNull: true }
+    { skipNull: true },
   );
 }
 
@@ -131,7 +131,7 @@ export function getAccountTypeColors(type: AccountTypes) {
 }
 
 export function countTransactionCategories(
-  transactions: Transaction[]
+  transactions: Transaction[],
 ): CategoryCount[] {
   const categoryCounts: { [category: string]: number } = {};
   let totalCount = 0;
@@ -160,7 +160,7 @@ export function countTransactionCategories(
       name: category,
       count: categoryCounts[category],
       totalCount,
-    })
+    }),
   );
 
   // Sort the aggregatedCategories array by count in descending order
@@ -195,8 +195,40 @@ export const getTransactionStatus = (date: Date) => {
   return date > twoDaysAgo ? "Processing" : "Success";
 };
 
-
-export const authformSchema = z.object({
-    email: z.string().email(),
-    password :z.string().min(8)
-})
+export const authformSchema = (type: string) =>
+  z.object({
+    //sign-up
+    firstName:
+      type === "sign-in"
+        ? z.string().optional()
+        : z.string().min(1, { message: "Required" }),
+    lastName:
+      type === "sign-in"
+        ? z.string().optional()
+        : z.string().min(1, { message: "Required" }),
+    address:
+      type === "sign-in"
+        ? z.string().optional()
+        : z.string().min(1, { message: "Required" }).max(50),
+    state:
+      type === "sign-in"
+        ? z.string().optional()
+        : z.string().min(1, { message: "Required" }).max(2),
+    postalCode:
+      type === "sign-in"
+        ? z.string().optional()
+        : z.string().min(1, { message: "Required" }).max(6),
+    dateOfBirth:
+      type === "sign-in"
+        ? z.string().optional()
+        : z.string().min(1, { message: "Required" }),
+    ssn:
+      type === "sign-in"
+        ? z.string().optional()
+        : z.string().min(1, { message: "Required" }),
+    //both
+    email: z.string().email("Invalid email"),
+    password: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters" }),
+  });
